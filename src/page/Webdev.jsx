c7f7692b2ec8367a71b761a9700a85330a9ffa35
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-06-23 18:38:09 +0100

    ww

--- src/page/Webdev.jsx
+++ src/page/Webdev.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import Webdesign from '../Component/CourseSelect/Webdesign/Webdesign'
-import Meant from '../Component/CourseSelect/Webdesign/Meant'
+import React from "react";
+import Webdesign from "../Component/CourseSelect/Webdesign/Webdesign";
+import Meant from "../Component/CourseSelect/Webdesign/Meant";
+import UserProfileCard from "../Card/Profilecard";
 
 function Webdev() {
+  const userStats = [
+    { label: "Articles", value: 13 },
+    { label: "Papers", value: 7 },
+    { label: "Followers", value: "2.5k" },
+  ];
   return (
     <div>
-        <Webdesign/>
-        <Meant/>
+      <Webdesign />
+      <Meant />
+      <div className="App">
+        <UserProfileCard
+          name="Kornebari Raphael Kpai"
+          desc="Hi, I'm Kornebari Raphael Kpai, a Computer Science teacher with a passion for graphic design. I hold a degree in Molecular Biology and Genetics from Eastern Mediterranean University Cyprus (TRNC). I'm excited to share my design skills with you in a way that's easy to understand and fun. Let's learn and create together!"
+          role="Graphic Designer"
+          image="/Assets/Image/Kornebari.jpg"
+          stats={userStats}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Webdev
+export default Webdev;
