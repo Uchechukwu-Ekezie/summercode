66527a55b70028d59d0279a519b544daae1a28d3
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-06-25 16:35:03 +0100

    dnd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      screens:{
-        sm:'480px',
-        md:'768px',
-        lg:'984px',
-        xl:'1440px'
-      },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "984px",
+      xl: "1440px",
     },
+    extend: {},
   },
   plugins: [],
-}
+};
