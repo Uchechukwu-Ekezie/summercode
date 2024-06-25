7d96a121f35eeb2d106636c3170aeb8162887489
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-06-25 12:42:06 +0100

    new

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Hero from '../Component/Hero/Hero'
+import ParentTest from '../Component/Testimoial/ParentTest'
+import Banner from '../Component/Banner/Banner'
 
 
 function Home() {
   return (
     <div>
         <Hero/>
+        <Banner/>
+        <ParentTest/>
     </div>
   )
 }
