a575a7f55879a2fa3cd709efb3b576810422e804
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-06-26 23:04:11 +0100

    gg

--- src/page/Cybersec.jsx
+++ src/page/Cybersec.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import UserProfileCard from '../Component/Card/Profilecard'
 
 function Cybersec() {
   return (
-    <div>Cybersec</div>
+    <div className='pt-32'>
+      <UserProfileCard
+        name="Agba Paschal Eifowe"
+        desc="Hi, I'm Agba Paschal Eifowe a recent computer science graduate with a passion for cybersecurity and education. With a solid foundation in computer science principles and hands-on experience in cybersecurity, Jane is dedicated to inspiring and educating young minds about the importance of cybersecurity in today's digital age.
+        "
+        role="Cyber Security Analyst"
+        image="/Assets/Image/agbapaschal.png"
+      />
+    </div>
   )
 }
 
