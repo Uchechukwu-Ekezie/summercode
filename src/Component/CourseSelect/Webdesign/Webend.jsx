b721582270d14568f3a299cde31dbd6b2524ef8c
ogazboiz <ogazboizakpolo@gmail.com>
2024-06-28 22:06:43 +0100

    done with the color

--- src/Component/CourseSelect/Webdesign/Webend.jsx
+++ src/Component/CourseSelect/Webdesign/Webend.jsx
@@ -8,7 +8,7 @@ function Webend() {
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
             Master Web Development.
-            <strong className="font-extrabold text-yellow-500 sm:block">
+            <strong className="font-extrabold text-[#48758E] sm:block">
               {" "}
               Build Your Future.{" "}
             </strong>
@@ -22,7 +22,7 @@ function Webend() {
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <Link
               to="/enroll"
-              className="block w-full rounded bg-yellow-500 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              className="block w-full rounded bg-[#48758E] px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
             >
               Enroll Now
             </Link>
