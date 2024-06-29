3392eacd30217c8f8b0ae0dced3993d3be8727f5
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-06-29 11:41:46 +0100

    ee

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -18,15 +18,15 @@ function Navigation({ Enroll }) {
   ]
 
   return (
-    <header className="fixed z-50 w-full shadow-md bg-[#48578E] ">
-      <div className="container flex items-center justify-between mx-auto my-2 md:px-6">
-        <Link to="/" className="flex items-center">
-          <img
-            src="/Assets/Image/Colorful_Brain_Human_Technology_Logo-removebg-preview.png"
-            alt="logo"
-            className="w-[40px] h-[40px] md:w-[100px] md:h-[100px] sm:w-[100px] sm:h-[100px]"
-          />
-        </Link>
+    <header className="fixed z-50 w-full shadow-md bg-[#48758E] ">
+    <div className="container flex items-center justify-between mx-auto my-2 md:px-6">
+      <Link to="/" className="flex items-center">
+        <img
+          src="/Assets/Image/Colorful_Brain_Human_Technology_Logo-removebg-preview.png"
+          alt="logo"
+          className="w-16 h-16 md:w-24 md:h-24"
+        />
+      </Link>
 
       <div className="md:hidden">
         <button
@@ -42,33 +42,33 @@ function Navigation({ Enroll }) {
         </button>
       </div>
 
-        <nav
-          className={`${
-            isOpen ? "translate-y-0" : "translate-y-full"
-          } `}
-        id="navitem">
-          <ul className="flex flex-col items-start justify-center h-full p-6 space-y-6 md:flex-row md:space-y-0 md:space-x-8 md:p-0">
-            {navItems.map((item) => (
-              <li key={item.id} className="md:border-none">
-                <NavLink
-                  to={item.link}
-                  className="block font-extrabold text-black transition-all hover:text-white md:text-base"
-                  onClick={toggleMenu}
-                >
-                  {item.text}
-                </NavLink>
-              </li>
-            ))}
-            <li className="mt-4 md:hidden">
-              <Link to="/contact">
-                <Button
-                  txt="Speak With Us"
-                  className="text-black bg-white rounded-xl"
-                />
-              </Link>
+      <nav
+        className={`${
+          isOpen ? "translate-y-0" : "translate-x-full"
+        } fixed top-14 right-0 w-2/4 h-full bg-[#48758E] transition-transform duration-300 ease-in-out md:static md:flex md:w-auto md:h-auto md:bg-transparent md:translate-x-0`}
+      >
+        <ul className="flex flex-col items-start justify-center h-full p-6 space-y-6 md:flex-row md:space-y-0 md:space-x-8 md:p-0">
+          {navItems.map((item) => (
+            <li key={item.id} className="md:border-none">
+              <NavLink
+                to={item.link}
+                className="block font-semibold transition-all text-[#f9f9fa] hover:font-bold md:text-base"
+                onClick={toggleMenu}
+              >
+                {item.text}
+              </NavLink>
             </li>
-          </ul>
-        </nav>
+          ))}
+          <li className="mt-4 md:hidden">
+            <Link to="/contact">
+              <Button
+                txt="Speak With Us"
+                className="text-black bg-white rounded-xl"
+              />
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
       <div className="hidden md:block">
         <Link to="/contact">
