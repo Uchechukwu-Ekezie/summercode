b134dbe73e21e5093824ae6025d633bafbda200f
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-07-01 21:48:20 +0100

    ff

--- src/Component/RegistrationForm/RegistrationForm.jsx
+++ src/Component/RegistrationForm/RegistrationForm.jsx
@@ -1,40 +1,73 @@
-import React from 'react';
+import React from "react";
 
 const RegistrationForm = () => {
   return (
     <div className="p-4">
-      <h1 className="mb-8 text-3xl font-extrabold text-center text-[#48758E] ">Enroll Now</h1>
+      <h1 className="mb-8 text-3xl font-extrabold text-center text-[#48758E] ">
+        Enroll Now
+      </h1>
       <div className="max-w-lg mx-auto overflow-hidden bg-white rounded-lg shadow-md">
         <div className="px-6 py-8">
           <form>
-          <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
-          Parent's First Name
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="firstName"
-          type="text"
-          placeholder="Parent's First Name"
-          required
-        />
-      </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="firstName"
+              >
+                Parent's First Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="firstName"
+                type="text"
+                placeholder="Parent's First Name"
+                required
+              />
+            </div>
 
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
-          Parent's Last Name
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="lastName"
-          type="text"
-          placeholder="Parent's Last Name"
-          required
-        />
-      </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="lastName"
+              >
+                Parent's Last Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="lastName"
+                type="text"
+                placeholder="Parent's Last Name"
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="children"
+              >
+                How many children are you enrolling?
+              </label>
+              <select
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="children"
+                required
+              >
+                <option value="">How many children are you enrolling?</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+
             <div className="mb-6">
-              <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">
-               Child Full Name
+              <label
+                htmlFor="fullName"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Child Full Name
               </label>
               <input
                 type="text"
@@ -46,8 +79,11 @@ const RegistrationForm = () => {
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-              Parent's Email Address
+              <label
+                htmlFor="email"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Parent's Email Address
               </label>
               <input
                 type="email"
@@ -59,7 +95,10 @@ const RegistrationForm = () => {
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="course" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="course"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
                 Select Course
               </label>
               <select
@@ -69,14 +108,19 @@ const RegistrationForm = () => {
                 required
               >
                 <option value="">Select a course</option>
-                <option value="webdev">Web Development with HTML, CSS, and JavaScript</option>
+                <option value="webdev">
+                  Web Development with HTML, CSS, and JavaScript
+                </option>
                 <option value="dataanalysis">Digital Media</option>
                 <option value="machinelearning">AI & Machine Learning</option>
                 <option value="cybersecurity">Cybersecurity </option>
               </select>
             </div>
             <div className="mb-6">
-              <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="message"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
                 Message (Optional)
               </label>
               <textarea
