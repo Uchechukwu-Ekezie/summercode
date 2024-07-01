b608950bd78d10d10c7c59a4ce28ab3b999231b1
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-07-01 23:47:16 +0100

    gg

---
+++ src/Component/Partnership/PartnerShipSection.jsx
@@ -0,0 +1,23 @@
+// PartnershipSection.js
+import React from "react";
+ // Adjust path as needed
+
+function PartnershipSection() {
+  return (
+    <div className="py-16 bg-gray-200">
+      <div className="px-4 mx-auto max-w-screen-2xl sm:px-6 lg:px-8">
+        <div className="text-center">
+          
+          <h2 className="text-3xl font-semibold text-gray-800 mb-4">
+            In Partnership with Saintspark Innovate
+          </h2>
+          <p className="text-lg text-gray-600">
+            Discover the latest innovations and opportunities through our partnership with Saintspark Innovate. Together, we bring you cutting-edge technology and educational experiences.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PartnershipSection;
